706950ba69e7d24ac02e79faa6ba6da447a20ecf
FPCITS <sara@freedompracticecoaching.com>
2022-06-26 22:56:37 -0500

    added modules and beginner questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require ('util');
+const generateMArkdown =
+require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type:'input',
+    name: 'title',
+    message: 'What would you like to title your repository?',
+    
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Enter your repository title');
+            return false;
+        }
+    }
+
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'What is the description of your repository?',
+    validate: nameInput => {
+        if (nameInput){
+            return true;
+        } else {
+            console.log('Enter a description of the repository.');
+            return false;
+        }
+    }
+
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init()
+
